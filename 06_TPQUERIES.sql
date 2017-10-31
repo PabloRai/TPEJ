7f61f8edd0b22290717d3124acc63b99d4419abe
Nina Satragno <nsatragno@gmail.com>
2017-10-31 15:42:17 -0300

    Clean up code
    Add ordering to files.
    Do not use dates in the future when inserting.

--- 06_TPQUERIES.sql
+++ 06_TPQUERIES.sql
@@ -1,4 +1,4 @@
 -- Indique las incidencias pendientes con más de 5 días de antigüedad
 SELECT S.IdIncidencia
 FROM SOLICITUD S
-WHERE DATEDIFF(DAY,S. FechaDeRealizacion, GETDATE()) > 5;
+WHERE DATEDIFF(DAY, S.FechaDeRealizacion, GETDATE()) > 5;
